96c8b8ae5fb8f2c664d2e0e53d1c3b6810c7b5b5
Yuliya K <36555259+yuliyakrygina@users.noreply.github.com>
2019-03-10 14:56:17 -0400

    Scripts

---
+++ Fight Scene/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed = 5f;
+	public float rspeed = 90f;
+	Rigidbody rb;
+	public GameObject bullet;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float xInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+		float yInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+		float zInput = Input.GetAxis("Jump") * rspeed * Time.deltaTime;
+
+		transform.Rotate(0, zInput, 0);
+
+		transform.Translate(xInput, 0, yInput);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Instantiate(bullet, transform.position, Quaternion.identity);
+
+		}
+	}
+
+}
